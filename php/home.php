62f86ee50ea6e72c6afa4a52133ba06414fb231e
mrCardizzle <shivamv@stud.ntnu.no>
2018-08-06 01:09:46 +0200

    Add css for create tournament

--- php/home.php
+++ php/home.php
@@ -1,3 +1,4 @@
-<form class="container" action="?page=create_tournament" method="post">
-  <input type="submit" name="ctButton" value="Create Tournament">
+<form  action="?page=create_tournament" method="post">
+<button type="submit" class="btn btn-primary btn-lg">Create tournament</button>  
+
 </form>
